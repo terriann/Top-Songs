b30a97bf21a4f81053cf0446e4f52070493a56ce
Terri Ann <terri.ann.s@gmail.com>
2014-10-17 00:15:50 -0400

    Pushing commented out segment musing about building pre-pend cards

--- www/src/js/views/app.view.js
+++ www/src/js/views/app.view.js
@@ -1,8 +1,25 @@
-  
+
   AppView = Backbone.View.extend({
     el: $("body"),
     initialize: function () {
       this.songs = new Songs( null, { view: new SongView() });
+
+        item = {
+/*
+                id: data.id.attributes['im:id'],
+                song_name: data['im:name'].label,
+                link: data['link'][0].attributes.href,
+                song_preview: data['link'][1].attributes.href,
+                artist_name: data['im:artist'].label,
+                images: {
+                    small: data['im:image'][0].label,
+                    medium: data['im:image'][1].label,
+                    large: data['im:image'][2].label
+
+                    I'll either have to follow this pattern or create a whole new one..... Think about it TAS 2014-10-17
+*/
+        };
+        this.songs.add( new Song(item) );
 
       	this.fetchJson();
 
